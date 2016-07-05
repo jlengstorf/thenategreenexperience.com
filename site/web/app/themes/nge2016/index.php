ed3f5f3f42e0d79b3c6cf6b0c21330fb33485199
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-05 15:25:10 +0200

    feat(footer-bio): added the footer bio to the blog preview display

--- site/web/app/themes/nge2016/index.php
+++ site/web/app/themes/nge2016/index.php
@@ -1,15 +1,35 @@
 
 <div class="content">
-<?php if (!have_posts()) : ?>
+<?php
+
+  if (!have_posts()):
+
+?>
   <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
+<?php
+
+    _e('Sorry, no results were found.', 'sage');
+
+?>
   </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
+<?php
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
+    get_search_form();
+  endif;
 
-<?php the_posts_navigation(); ?>
+  while (have_posts()) {
+    the_post();
+
+    $part = get_post_type() !== 'post' ? get_post_type() : get_post_format();
+    get_template_part('templates/content', $part);
+  }
+
+  the_posts_navigation([
+    'prev_text' => '&lsaquo; older posts',
+    'next_text' => 'newer posts &rsaquo;'
+  ]);
+
+  get_template_part('templates/footer', 'bio');
+
+?>
 </div>
