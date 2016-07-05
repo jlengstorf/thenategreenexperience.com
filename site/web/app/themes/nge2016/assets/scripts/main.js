2f5753005e2dbcf033834a084b90741066fed3de
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-05 15:23:32 +0200

    feat(footnotes): added JS scrolling to footnotes

--- site/web/app/themes/nge2016/assets/scripts/main.js
+++ site/web/app/themes/nge2016/assets/scripts/main.js
@@ -1,3 +1,14 @@
 import { lazyLoadImages } from 'responsive-lazyload';
+import { scrollToLocalLinks } from './utils/scroll-to-anchor';
+import { highlightCurrentFootnote } from './blocks/footnotes';
 
 lazyLoadImages();
+
+document.addEventListener('click', event => {
+  scrollToLocalLinks(event, {
+    offsetAdjustment: 140,
+    callback: highlightCurrentFootnote.bind(null, {
+      containerClass: 'footnotes',
+    }),
+  });
+});
