e5f894ec63eadab12700332bbd75cdc9c1e2d0f1
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-05 15:25:52 +0200

    feat(single): added footnotes and the footer bio

--- site/web/app/themes/nge2016/templates/content-single.php
+++ site/web/app/themes/nge2016/templates/content-single.php
@@ -1,17 +1,30 @@
+<?php
+
+use NGE\Custom\Footnotes;
+
+?>
 <div class="content">
-<?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+<?php
+
+  while (have_posts()):
+    the_post();
+
+?>
+  <article class="article">
+    <header class="article__header">
+      <h1 class="article__heading"><?php the_title(); ?></h1>
     </header>
-    <div class="entry-content">
+    <div class="article__content">
       <?php the_content(); ?>
     </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <div class="comments"><?php comments_template('/templates/comments.php'); ?></div>
   </article>
-<?php endwhile; ?>
+<?php
+
+  endwhile;
+
+  get_template_part('templates/footer', 'bio');
+
+?>
+  <div class="footnotes"><?= Footnotes\footnotes() ?></div>
+  <div class="comments"><?php comments_template('/templates/comments.php'); ?></div>
 </div>
