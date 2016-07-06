409743d9d5d037ea2dfc9b61cc564d128bcca8e6
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-06 12:51:11 +0200

    feat(typography): added Typekit fonts

--- site/web/app/themes/nge2016/templates/head.php
+++ site/web/app/themes/nge2016/templates/head.php
@@ -5,4 +5,14 @@
   <?php if (is_admin_bar_showing()): ?>
   <style>#wpadminbar{margin-top:0;}.site__header{top:46px}@media screen and (min-width: 783px){.site__header{top:32px}}</style>
   <?php endif; ?>
+  <script>
+    (function(d) {
+      var config = {
+        kitId: 'efz1nfr',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  </script>
 </head>
