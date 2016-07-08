1f9254de2506f23d2de79cfdf851385a910e3efb
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-08 15:29:16 +0200

    fix(front-page): updated the home page layout
    The hero box is now full-height and supported by custom fields for
    editing.
    
    Below the hero, we removed the blog previews and instead added
    normal page content.
    
    fixes #1, #9

--- site/web/app/themes/nge2016/front-page.php
+++ site/web/app/themes/nge2016/front-page.php
@@ -5,36 +5,13 @@
 
 ?>
 <div class="nate-intro">
-  <h1 class="nate-intro__heading"><?php the_title(); ?></h1>
-  <div class="nate-intro__text"><?php the_content(); ?></div>
+  <h1 class="nate-intro__heading"><?php the_field('heading'); ?></h1>
+  <div class="nate-intro__text"><?php the_field('cta_text'); ?></div>
 </div>
-
-<?php
-
-    $query = new WP_Query([ 'post_type' => 'post', 'posts_per_page' => 4 ]);
-    if ($query->have_posts()):
-
-?>
 <div class="content content--front-page">
-<?php
+<?php the_content(); ?>
 
-      while($query->have_posts()) {
-        $query->the_post();
-        get_template_part('templates/post', 'preview');
-      }
-
-?>
-  <div class="pagination">
-    <a href="<?= home_url('/blog/') ?>" class="pagination__link button">More Blog Posts &rsaquo;</a>
-  </div>
 </div>
 <?php
 
-    endif;
-
-?>
-
-<?php
-
-    wp_reset_postdata();
   endwhile;
