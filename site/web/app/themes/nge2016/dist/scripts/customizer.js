6421639f01b3a38365131971842b4069c30b8324
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-09 16:45:06 +0200

    feat(deploy): updated staging and prod names, fixed asset deployment
    No longer committing the `dist` folder, now that I've realized
    there's a way to build and copy the files during deploys.

--- site/web/app/themes/nge2016/dist/scripts/customizer.js
+++
@@ -1,1 +0,0 @@
-!function(t){function n(e){if(r[e])return r[e].exports;var o=r[e]={exports:{},id:e,loaded:!1};return t[e].call(o.exports,o,o.exports,n),o.loaded=!0,o.exports}var r={};return n.m=t,n.c=r,n.p="",n(0)}([function(t,n){"use strict";!function(t){wp.customize("blogname",function(n){n.bind(function(n){t(".brand").text(n)})})}(jQuery)}]);
