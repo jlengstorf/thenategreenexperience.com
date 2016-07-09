5d7ed57734943eede5ba13fb3fc64088c0feb0e8
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-09 23:54:10 +0200

    fix(peeker): button text shows correctly now

--- site/web/app/themes/nge2016/templates/peeker.php
+++ site/web/app/themes/nge2016/templates/peeker.php
@@ -3,7 +3,7 @@
     <h3 class="peeker__heading"><?php the_field('peeker_heading', 'options'); ?></h3>
     <div class="peeker__text"><?php the_field('peeker_text', 'options'); ?></div>
   </div>
-  <?= do_shortcode('[opt-in class="peeker__form-wrap" button_text="' . get_field('peeker_button') . '"]') ?>
+  <?= do_shortcode('[opt-in class="peeker__form-wrap" button_text="' . get_field('peeker_button', 'options') . '"]') ?>
   <div class="peeker__close">
     <a href="#close-peeker" class="peeker__close-link">close &times;</a>
   </div>
