325ebc824e11aaede355732ecbf853da7634bc0d
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-12 19:41:35 +0200

    feat(intro): made the "see what..." text clickable
    Page scrolls down on click now.
    
    fixes #15

--- site/web/app/themes/nge2016/front-page.php
+++ site/web/app/themes/nge2016/front-page.php
@@ -7,8 +7,10 @@
 <div class="nate-intro">
   <h1 class="nate-intro__heading"><?php the_field('heading'); ?></h1>
   <div class="nate-intro__text"><?php the_field('cta_text'); ?></div>
+  <a href="#more"
+     class="nate-intro__read-more">See What My Readers Have To Say</a>
 </div>
-<div class="content content--front-page">
+<div id="more" class="content content--front-page">
 <?php the_content(); ?>
 
 </div>
