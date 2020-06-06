ae3e9efdd36b72f092e6ebb470583b0ab01828b9
Jason Lengstorf <jason@lengstorf.com>
2020-06-06 14:28:52 -0700

    feat: add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -45,6 +45,27 @@ const Layout = ({ children, newsletter = true }) => {
           media="print"
           onload="this.media='all'"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#031782" />
+        <meta name="msapplication-TileColor" content="#031782" />
+        <meta name="theme-color" content="#031782"></meta>
       </Helmet>
       <Header />
       <main className={styles.main}>{children}</main>
